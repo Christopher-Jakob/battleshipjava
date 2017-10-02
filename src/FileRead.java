e508082f01073d04306906f8a07cd5db5cf69556
Christopher Jakob <christopher.m.jakob@gmail.com>
2017-10-01 23:40:23 -0500

    changes before bed

---
+++ src/FileRead.java
@@ -0,0 +1,18 @@
+/**
+ * Created by rickus on 10/1/17.
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileRead {
+    private File file;
+
+    public FileRead(String f ){
+         file = new File(f);
+
+    }
+
+
+} //Fileread end
--- src/tests/TestFileTest.java
+++ src/tests/TestFileTest.java
@@ -1,16 +1,24 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Created by rickus on 10/1/17.
- */
+
 public class TestFileTest {
     @org.junit.Test
     public void readfile(){
-        TestFileTest greaterthree = new TestFileTest("/filesfortests/greaterthree.txt");
+        TestFileTest greaterthree = new TestFile("./filesfortests/greaterthree.txt");
         assetEquals("more than 3 lines", 1, greaterthree.readfile());
+
+        TestFileTest lessthree = new TestFile("./filefortests/lessthree.txt");
+        assertEquals("less than 3 lines", 1, lessthree.readfile());
+
+        TestFileTest invalidcoordinate = new TestFile("./filefortests/invalidcoordinate.txt");
+        assertEquals("invalid coordinate format", 1, invalidcoordinate.readfile());
+
+        TestFileTest validfile = new TestFile("./filesfortests/validtest.txt");
+        assertEquals("valid file test pass", 0, validfile.readfile());
     }
+
 
 }
 
